4c61404cbe1c1869d36e9bd7ef90b85fe0b1e5c7
Megha rajak <mmegharajak98@gmail.com>
2026-09-11 18:40:24 +0530

    Read integer input using scanner

--- J01_JavaBasics/C05_Input.java
+++ J01_JavaBasics/C05_Input.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class C05_Input {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-       // String input = sc.next();
+       // String input = scanner.next();
        // System.out.println(input);
 
        //String name = sc.nextLine();
