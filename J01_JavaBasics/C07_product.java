bb9883c367e2844dc74111db47ce16b4e2bc080d
Megha rajak <mmegharajak98@gmail.com>
2026-09-13 00:59:32 +0530

    practice multiplying two Scanner inputs

--- J01_JavaBasics/C07_product.java
+++ J01_JavaBasics/C07_product.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class C07_product {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int product = a * b;
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float product = a * b;
         System.out.println(product);
     }
 }
---
+++ J01_JavaBasics/C08_area.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class C08_area {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        float length = sc.nextFloat();
+        float width = sc.nextFloat();
+        float area = length * width;
+        System.out.println(area);
+    }
+}
