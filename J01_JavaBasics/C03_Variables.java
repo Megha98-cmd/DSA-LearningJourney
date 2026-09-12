727c94a1581bbac1d9beafdcdb3ee4b49d854099
Megha rajak <mmegharajak98@gmail.com>
2026-09-12 22:23:14 +0530

    add String variable

--- J01_JavaBasics/C03_Variables.java
+++ J01_JavaBasics/C03_Variables.java
@@ -2,13 +2,13 @@ public class C03_Variables {
     public static void main(String args[]) {
         int a = 10;
         int b = 5;
-        System.out.println(a);
-        System.out.println(b);
-        String name = "Megha Rajak";
-        System.out.println(name);
+        //System.out.println(a);
+        //System.out.println(b);
+        //String name = "Megha Rajak";
+        //System.out.println(name);
 
 
         a = b;
-        System.out.println(a);
+        //System.out.println(a);
     }
 }
