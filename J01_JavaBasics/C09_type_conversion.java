9676244ba398ed60e87aadd65df854bab49f472d
Megha rajak <mmegharajak98@gmail.com>
2026-09-13 01:43:38 +0530

    practice type conversion

---
+++ J01_JavaBasics/C09_type_conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class C09_type_conversion {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        float number = sc.nextInt(); 
+        System.out.println(number);
+
+       
+    }
+}
