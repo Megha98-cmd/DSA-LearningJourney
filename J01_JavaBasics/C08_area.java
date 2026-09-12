26b5e83296f4d7c3739b25aa62e29274f9fb1e5a
Megha rajak <mmegharajak98@gmail.com>
2026-09-13 01:03:00 +0530

    calculate area using float

--- J01_JavaBasics/C08_area.java
+++ J01_JavaBasics/C08_area.java
@@ -3,9 +3,8 @@ import java.util.*;
 public class C08_area {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        float length = sc.nextFloat();
-        float width = sc.nextFloat();
-        float area = length * width;
+        float radius = sc.nextFloat();
+        float area = 3.14f * radius * radius;
         System.out.println(area);
     }
 }
