07ef3e36090a11fbb2c80f7c5c4b7bfbad681437
Megha rajak <mmegharajak98@gmail.com>
2026-09-14 23:49:32 +0530

    practicing arithmetic operator

---
+++ J02_operators/C01_arithmetic_operator.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class C01_arithmetic_operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
