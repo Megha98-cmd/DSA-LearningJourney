2dc7e55786d690940f2658aacb889e359d0b9abd
Megha rajak <mmegharajak98@gmail.com>
2026-09-18 23:23:54 +0530

    practicing assignment operator

---
+++ J02_operators/C04_assignment_operator.java
@@ -0,0 +1,21 @@
+public class C04_assignment_operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        a += b; // a = a + b
+        System.out.println("a += b: " + a);
+
+        a -= b; // a = a - b
+        System.out.println("a -= b: " + a);
+
+        a *= b; // a = a * b
+        System.out.println("a *= b: " + a);
+
+        a /= b; // a = a / b
+        System.out.println("a /= b: " + a);
+
+        a %= b; // a = a % b
+        System.out.println("a %= b: " + a);
+    }
+}
