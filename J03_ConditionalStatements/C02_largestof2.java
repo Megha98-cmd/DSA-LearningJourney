d59d2cdf1ab94150a67f7ed7ca516051c04d8425
Megha rajak <mmegharajak98@gmail.com>
2026-09-23 23:17:05 +0530

    find largest of two numbers using if-else

--- J03_ConditionalStatements/C02_largestof2.java
+++ J03_ConditionalStatements/C02_largestof2.java
@@ -5,12 +5,12 @@ public class C02_largestof2 {
         int num1 = 10;
         int num2 = 20;
 
-        //if (num1 > num2) {
-          //  System.out.println(num1 + " is the largest number.");
-        //} else if (num2 > num1) {
-          //  System.out.println(num2 + " is the largest number.");
-        //} else {
-        //   System.out.println("Both numbers are equal.");
-        //  }
+        if (num1 > num2) {
+            System.out.println(num1 + " is the largest number.");
+        } else if (num2 > num1) {
+            System.out.println(num2 + " is the largest number.");
+        } else {
+            System.out.println("Both numbers are equal.");
+        }
     }
 }
